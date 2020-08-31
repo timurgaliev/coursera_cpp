3a6ce893d4d0a3b25e0035dcfce18174e0d3ae42
timurgaliev <samolet775@gmail.com>
2020-08-31 13:15:34 +0300

    Add files via upload

---
+++ Coursera Yellow Belt/decomposition/bus_manager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "responses.h"
+
+using namespace std;
+
+
+class BusManager {
+public:
+    void AddBus(const string& bus, const vector<string>& stops);
+
+    BusesForStopResponse GetBusesForStop(const string& stop) const;
+
+    StopsForBusResponse GetStopsForBus(const string& bus) const;
+
+    AllBusesResponse GetAllBuses() const;
+
+private:
+    map<string, vector<string>> buses_to_stops;
+    map<string, vector<string>> stops_to_buses;
+};

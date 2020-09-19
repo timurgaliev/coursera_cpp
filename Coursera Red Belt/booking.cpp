696bfd60c48ec7496e87ae46f5c05fd72d33be58
timurgaliev <samolet775@gmail.com>
2020-09-20 01:26:08 +0300

    Update booking.cpp

--- Coursera Red Belt/booking.cpp
+++ Coursera Red Belt/booking.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <deque>
 using namespace std;
+
+struct IncomingParameters{
+    int64_t time;
+    string hotel_name;
+    int client_id;
+    int room_count;
+};
 
 class Booking{
 public:
-    void Book(int64_t time,string hotel_name,int client_id,int room_count);
+    void Book(int64_t time_,string hotel_name_,int client_id_,int room_count_){
+        data.push_back({time_,hotel_name_, client_id_, room_count_});
+        while(time_ - data.begin()->time >= 86400){
+            data.pop_front();
+        }
+    }
     int Clients(string hotel_name);
     int Rooms(string hotel_name);
 private:
-    
+    deque<IncomingParameters> data; 
 
 };
 
